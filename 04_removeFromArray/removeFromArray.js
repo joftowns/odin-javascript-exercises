5193bba2124f0d537d43f90ea96ed02f15fede2f
John Townsend <22449384+joftowns@users.noreply.github.com>
2023-07-07 21:08:27 +0900

    Pass 1st test on exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, deletion) {
+	let target = array.indexOf(deletion);  //finds the index of "deletion" element in the array
+	array.splice(target, 1); //at that index, removes 1 element
+	return array; //returns the new array
 };
+
+
+//const foo = function () {
+//	for (let i = 0; i < arguments.length; i++) {
+//		console.log(arguments[i]);
+//	}
+//}
 
 // Do not edit below this line
 module.exports = removeFromArray;
