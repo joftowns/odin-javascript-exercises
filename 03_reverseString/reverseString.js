57208a63932c04edfd0fe28ab2923d98d174030b
John Townsend <22449384+joftowns@users.noreply.github.com>
2023-07-07 20:41:28 +0900

    Pass all tests of exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+	let reversedPhrase = '';
+	for (let i = 1; i <= phrase.length; i++) {
+		let letter = phrase.substr(-i, 1)
+		reversedPhrase += letter;
+	}
+	return reversedPhrase;
 };
+
+//their solution is definitely cleaner!
+//that is: phrase.split('').reverse().join('')
+//.split splits each character into an array
+//.reverse() reverses the array
+//.join('') joins the elements of the array into a string
+//separated by nothing ('')
 
 // Do not edit below this line
 module.exports = reverseString;
