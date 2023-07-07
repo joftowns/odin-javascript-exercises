27fe2fd1fef3164c92bba48dcb44e5a8da0a08b3
John Townsend <22449384+joftowns@users.noreply.github.com>
2023-07-07 16:18:38 +0900

    Pass 4 tests in exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let finalString = '';
+	for (let i = 0; i < num; i++) {
+		finalString = finalString + string;
+	}
+	return finalString;	
 };
+
+// output a string composed of 'string' concatenated num times
+
 
 // Do not edit below this line
 module.exports = repeatString;
