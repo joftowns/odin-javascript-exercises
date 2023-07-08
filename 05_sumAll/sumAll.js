d3f925e174ed5a7b9262c0c94a6ac79fc1d1fad3
John Townsend <22449384+joftowns@users.noreply.github.com>
2023-07-08 19:11:28 +0900

    Pass all tests on exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  //swaps the values if a is larger than b
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
 
+  //returns an error if either value is negative
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+    //this seems like a silly way to do this but it worked
+  } else if ( !(typeof a === "number" && typeof b === "number") ) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  };
+
+  return sum;
 };
 
 // Do not edit below this line
